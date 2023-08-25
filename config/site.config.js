2d9f6f4e25edc9c7ab07df516b9835b0cb59d11c
纸织 <98130008+XFYPER@users.noreply.github.com>
2023-08-26 04:05:02 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -44,7 +44,6 @@ module.exports = {
   // the directories in which you have .password set. Check the documentation for details.
   protectedRoutes: [
     '/',
-    '/test',
                    ],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
